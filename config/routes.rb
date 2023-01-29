751b4cd54a3ce40566f3f773f5be432c2fed6fc7
825814 <120297399+825814@users.noreply.github.com>
2023-01-29 13:46:39 +0000

    [add]devise　名前　ログイン

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get 'books/show'
   get 'books/edit'
   root to: "homes#top"
+  get 'home/about' => "homes#about"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
