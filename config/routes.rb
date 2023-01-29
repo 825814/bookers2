ad1eb2e356e410520f6d2f1c720c512e4b2cd3f1
825814 <120297399+825814@users.noreply.github.com>
2023-01-29 14:05:43 +0000

    [add]アソシエーション

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
+  
   root to: "homes#top"
+  
+  resources :books, only: [:index, :show, :edit]
+  
   get 'home/about' => "homes#about"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
