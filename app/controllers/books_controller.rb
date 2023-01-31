49802d0ebfa267fcdcaddcb77b1831758ccb2035
825814 <120297399+825814@users.noreply.github.com>
2023-01-31 11:28:02 +0000

    [add]投稿できる

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,27 @@
 class BooksController < ApplicationController
-  def index
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to books_path
   end
+
+  def index
+    @book = Book.new
+    @books = Book.all
+  end
+
 
   def show
   end
 
   def edit
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,12 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
 
 
+　def get_image
+　  unless  image.attached?
+　    file_path = Rails.root.join('app/assets/images/no_image.jpg')
+　    image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+　  end
+　  image
+　end
+
 end
