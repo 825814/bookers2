ffaefca23d23c87708d9ec13155be241e00ebc4f
825814 <120297399+825814@users.noreply.github.com>
2023-01-31 14:27:35 +0000

    [add]booksのshowを作成中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,7 +4,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
-    redirect_to books_path
+    redirect_to book_path(book.id)
   end
 
   def index
@@ -14,6 +14,7 @@ class BooksController < ApplicationController
 
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
