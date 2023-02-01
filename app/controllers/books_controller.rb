54cd0db31615c6854ae1b1b01bfa5f6311901e10
825814 <120297399+825814@users.noreply.github.com>
2023-02-01 10:29:18 +0000

    [add]アプリケーション名の変更

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -20,9 +20,9 @@ class BooksController < ApplicationController
 
   def edit
   end
-  
+
   def destroy
-  　@book = Book.find(params[:id])
+    @book = Book.find(params[:id])
     @book.destroy
     redirect_to books_path
   end
--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Boookers2
+module Bookers2
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
