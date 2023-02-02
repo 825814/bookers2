9ad1abe73a4c297591a453d4f9305740c45931af
825814 <120297399+825814@users.noreply.github.com>
2023-02-02 06:23:56 +0000

    [add]プロフィール画像の設定の途中

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,15 +4,31 @@ class UsersController < ApplicationController
   
   def index
     @users = User.all
+   
   end
+  
+
 
   def show
-    @user = User.new
+    @user = User.find(params[:id])
     @books = @user.books
   end
 
   def edit
     @user = User.find(params[:id])
-    
   end
+  
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
+  end
+  
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  
+
   devise_for :users
 
   root to: "homes#top"
 
   resources :books, only: [:create, :index, :show, :edit, :destroy]
-  resources :users, only: [:index, :show, :edit]
-  
+  resources :users, only: [:index, :show, :edit, :update]
+
   get 'home/about' => "homes#about"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
