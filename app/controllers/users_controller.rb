25aa353b26a26c67a36d5b3c1b392372ef3d771c
825814 <120297399+825814@users.noreply.github.com>
2023-02-02 13:29:18 +0000

    [add]投稿できるようになった

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def index
     @users = User.all
     @book = Book.new
+    @book_new = Book.new
   end
 
 
@@ -17,6 +18,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @books = @user.books
+    @book_new = Book.new
   end
 
   def edit
