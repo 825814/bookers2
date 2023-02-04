6e1bc9919d08dd90238c89c399fd1148d19c281b
825814 <120297399+825814@users.noreply.github.com>
2023-02-04 14:45:42 +0000

    [add]フラッシュメッセージ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,6 +4,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
+    flash[:notice] = "You have created book successfully."
     redirect_to book_path(@book.id)
   else
     @books = Book.all
@@ -30,6 +31,7 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
+    flash[:notice] = "You have updated book successfully."
     redirect_to book_path(@book.id)
   else
     render :edit
