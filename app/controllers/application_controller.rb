38146e5e5954a2fd64a3597f5b4c98c6199fb968
825814 <120297399+825814@users.noreply.github.com>
2023-02-04 02:55:50 +0000

    [add]フラッシュメッセージ作成中

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,17 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!, except: [:top]
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+
+
+
+
+
   def after_sign_in_path_for(resource)
     books_path
   end
 
   def after_sign_out_path_for(resource)
-    home_about_path
+    root_path
   end
 
   protected
