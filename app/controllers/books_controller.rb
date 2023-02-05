8c3eba73b3171b3091d21cee49b80029809dec46
825814 <120297399+825814@users.noreply.github.com>
2023-02-05 11:10:29 +0000

    [add]プロフィール編集ページのリンク

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -15,6 +15,7 @@ class BooksController < ApplicationController
   def index
     @book = Book.new
     @books = Book.all
+
   end
 
 
--- app/models/book.rb
+++ app/models/book.rb
@@ -6,11 +6,11 @@ class Book < ApplicationRecord
   validates :title, presence: true
   validates :body, presence: true
 
-  def get_profile_image
+  def get_profile_image(width, height)
     unless profile_image.attached?
       file_path = Rails.root.join('app/assets/images/sample-author1.jpg')
       profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
-    profile_image.variant(resize_to_limit: [100, 100]).processed
+    profile_image.variant(resize_to_limit: [width, height]).processed
   end
 end
