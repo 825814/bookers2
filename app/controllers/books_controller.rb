c59acc11f287eb04fab41c99136eb314e3a6c0aa
825814 <120297399+825814@users.noreply.github.com>
2023-02-08 09:42:33 +0000

    [update]アクセス制限（他人の投稿編集画面）

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,5 @@
 class BooksController < ApplicationController
+before_action :is_matching_login_user, only: [:edit, :update]
 
   def create
     @book = Book.new(book_params)
@@ -27,12 +28,14 @@ class BooksController < ApplicationController
   end
 
   def edit
-
+    # is_matching_login_user
 
     @book = Book.find(params[:id])
   end
 
   def update
+    # is_matching_login_user
+    
     @book = Book.find(params[:id])
     if @book.update(book_params)
     flash[:notice] = "You have updated book successfully."
@@ -53,4 +56,12 @@ class BooksController < ApplicationController
   def book_params
     params.require(:book).permit(:title, :body)
   end
+  
+  def is_matching_login_user
+    user_id = params[:id].to_i
+    unless user_id == current_user.id
+      redirect_to books_path
+    end
+  end
+  
 end
