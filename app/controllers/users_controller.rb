1733b11267f4f59f0f654e442f711e3b176e6494
825814 <120297399+825814@users.noreply.github.com>
2023-03-19 03:32:00 +0000

    [add]bookers2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -61,6 +61,6 @@ class UsersController < ApplicationController
   #     redirect_to user_path(current_user)
   #   end
   # end
-
+s
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
+
   root to: "homes#top"
 
   resources :books, only: [:create, :index, :show, :edit, :update, :destroy]
